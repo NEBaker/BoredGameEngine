0156e485f22e87b9fec27d58986ff4b3f3a3c699
NEBaker <noahbaker1@gmail.com>
2015-03-08 14:17:56 -0700

    Connect Four Added

--- src/ConnectFour.java
+++ src/ConnectFour.java
@@ -14,15 +14,15 @@ import javax.swing.JLabel;
  * Noah Baker CPE 102-03
  */
 
-public class Othello extends JComponent {
+public class ConnectFour extends JComponent {
 	private static int[][] board = new int[8][8];
-	private static int GameState = -1;
+	private static int GameState = 0;
 	private static int PlayerTurn = -1;
 	private static int tileWidth = 0;
 	private static int tileDiff = 5;
 	final static int EMPTY = 0;
 	final static int BLACK = 1;
-	final static int WHITE = -1;
+	final static int RED = -1;
 	final static int NORTH = -1;
 	final static int SOUTH = 1;
 	final static int WEST = -1;
@@ -35,13 +35,6 @@ public class Othello extends JComponent {
 		//places initial pieces on board
 		for(i=0;i<8;i++){ //Row
 			for(j=0;j<8;j++){ //Column
-				if ((j ==3 && i ==3) ||(j ==4 && i ==4)){
-					board[i][j] =BLACK; 
-				}
-				else if((j ==3 && i ==4) ||(j ==4 && i ==3)){
-					board[i][j] =WHITE; 
-				}
-				else
 					board[i][j] =EMPTY; 
 				//for debugging
 				System.out.print(board[i][j]);
@@ -49,13 +42,14 @@ public class Othello extends JComponent {
 			//for debugging
 			System.out.println();
 		}
-		//sets up the button configuration for othello
+		//sets up the button configuration for ConnectFour
 	}
 	
 	//checks to see if a move is possible, if so makes the move and returns 1
 	public static int  attemptMove(String ij){
 		int i,j;
-		boolean validMove = false;
+		int movesLeft =0;
+		boolean ValidMove = false;
 		
 		//parses the string
 		int Row = Integer.parseInt(ij.substring(0, 1)) ;
@@ -66,13 +60,17 @@ public class Othello extends JComponent {
 		System.out.println("Row: " +Row + " Column: " +  Column);
 
 		//check if a piece exists in the clicked location
-		if(board[Row][Column] == BLACK ||board[Row][Column] ==  WHITE){
+		if(board[Row][Column] == BLACK ||board[Row][Column] ==  RED){
 			return GameState; // if so returns 
 		}
 				
-		validMove = checkMove(Row,Column,true);
-	
-		if(validMove == true){ //checks to see if a valid move was made
+		ValidMove = checkMove(Row,Column,true);
+		
+		if(checkWin(Row,Column) ==true){
+			GameState = PlayerTurn;
+		}
+		
+		if(ValidMove == true){ //checks to see if a valid move was made
 			board[Row][Column] = PlayerTurn;
 			PlayerTurn *= -1; //passes the turn to the next player
 		}
@@ -87,7 +85,7 @@ public class Othello extends JComponent {
 			System.out.println();
 		}
 		*/
-		
+		/*
 		//checks to see if next player has any possible moves, if not, gives the turn to the player that played
 		for(i=0;i<8;i++){ //Row
 			for(j=0;j<8;j++){ //Column
@@ -102,206 +100,205 @@ public class Othello extends JComponent {
 		
 		System.out.println("no moves for player" + PlayerTurn);
 		PlayerTurn *= -1; //passes the turn to the next player
-		
+		*/
 		//checks if they have moves
+		
 		for(i=0;i<8;i++){ //Row
 			for(j=0;j<8;j++){ //Column
 				if(board[i][j] == EMPTY){
-					if(checkMove(i,j,false) == true){
-						System.out.println(" player has possible moves");
-						return GameState;
-					}
+					movesLeft++;
 				}
 			}
 		}
-		GameState = 0;
+		if(movesLeft ==0){
+			GameState = 2;
+		}
 		
-		
-		
-	 
 		return GameState;
 	}
 	
 	private static boolean checkMove(int Row, int Column, boolean makeMove){
+		if((Column + SOUTH == 8) || (board[Row][Column + SOUTH] != 0)){
+			   return true;
+		}
+		return false;
+	}
+	
+	private static boolean checkWin(int Row, int Column){
 		int i,j;
-		boolean validMove = false;
-		//checks if the move is valid and makes it if it is
+		boolean winMove = false;
+		int inArow = 0;
+		
 		south:
-		if((Column + SOUTH <= 7) && (board[Row][Column + SOUTH] == -1*PlayerTurn)){
+		if((Column + SOUTH <= 7) && (board[Row][Column + SOUTH] == PlayerTurn)){
+			inArow++;
 			for(j=Column + SOUTH;j <= 7;j += SOUTH){	//Column
 				if(board[Row][j] == PlayerTurn){ 		// looks for a piece to flip between
-					if(makeMove ==true){
-						for(j=j; j >Column; j -= SOUTH){ 	//flip pieces between player color and placed piece
-							board[Row][j] = PlayerTurn;
-						}
-					}
-					validMove = true;
-					break south;
+					inArow++;
 				}
 				else if(board[Row][j] == EMPTY){ 		//if an empty space is found none are flipped
 					break south;
 				}
+				if(inArow == 4){
+					winMove = true;
+				}
 			}
 		}
 		
+		inArow =0;
+
 		north:
-		if((Column + NORTH >= 0) && (board[Row][Column + NORTH] == -1*PlayerTurn)){
+		if((Column + NORTH >= 0) && (board[Row][Column + NORTH] == PlayerTurn)){
+			inArow++;
 			for(j=Column + NORTH;j >= 0;j += NORTH){ 	//Column
 				if(board[Row][j] == PlayerTurn){ 		// looks for a piece to flip between
-					if(makeMove ==true){
-						for(j=j; j <Column; j -= NORTH){	//flip pieces between player color and placed piece
-							board[Row][j] = PlayerTurn;
-							}
-						}
-					validMove = true;
-					break north;
+					inArow++;
 				}
 				else if(board[Row][j] == EMPTY){		//if an empty space is found none are flipped
 					break north;
 				}
+				if(inArow == 4){
+					winMove = true;
+				}
 			}
 		}
 		
+		inArow =0;
+
 		northwest:
-		if((Column + NORTH >= 0) && (Row + WEST >= 0) && (board[Row+WEST][Column + NORTH] == -1*PlayerTurn)){
+		if((Column + NORTH >= 0) && (Row + WEST >= 0) && (board[Row+WEST][Column + NORTH] == PlayerTurn)){
+			inArow++;
 			i = Row+WEST;															//first spot to start checking the row
 			for(j=Column + NORTH;j >= 0;j += NORTH){ 	//Column
 				if(i<0){																	// if the row is outside the array: stop
 					break northwest;
 				}
 				if(board[i][j] == PlayerTurn){ 		// looks for a piece to flip between
-					if(makeMove ==true){
-						for(j=j; j <Column; j -= NORTH){	//flip pieces between player color and placed piece
-							board[i][j] = PlayerTurn;
-							i-=WEST;
-						}
-					}
-					validMove = true;
-					break northwest;
+					inArow++;
 				}
 				else if(board[i][j] == EMPTY){		//if an empty space is found none are flipped
 					break northwest;
 				}
 				i += WEST;																//Increments the row
+				if(inArow == 4){
+					winMove = true;
+				}
 			}
 		}
+		
+		inArow =0;
 		
 		northeast:
-		if((Column + NORTH >= 0) && (Row + EAST <= 7) && (board[Row+EAST][Column + NORTH] == -1*PlayerTurn)){
+		if((Column + NORTH >= 0) && (Row + EAST <= 7) && (board[Row+EAST][Column + NORTH] == PlayerTurn)){
+			inArow++;
 			i = Row+EAST;															//first spot to start checking the row
 			for(j=Column + NORTH;j >= 0;j += NORTH){ 	//Column
 				if(i>7){																	// if the row is outside the array: stop
 					break northeast;
 				}
 				if(board[i][j] == PlayerTurn){ 		// looks for a piece to flip between
-					if(makeMove ==true){
-						for(j=j; j <Column; j -= NORTH){	//flip pieces between player color and placed piece
-							board[i][j] = PlayerTurn;
-							i-=EAST;
-						}
-					}
-					validMove = true;
-					break northeast;
+					inArow++;
 				}
 				else if(board[i][j] == EMPTY){		//if an empty space is found none are flipped
 					break northeast;
 				}
 				i += EAST;																//Increments the row
+				if(inArow == 4){
+					winMove = true;
+				}
 			}
 		}
 			
+		inArow =0;
+		
 		southwest:
-		if((Column + SOUTH <= 7) && (Row + WEST >= 0) && (board[Row+WEST][Column + SOUTH] == -1*PlayerTurn)){
+		if((Column + SOUTH <= 7) && (Row + WEST >= 0) && (board[Row+WEST][Column + SOUTH] == PlayerTurn)){
+			inArow++;
 			i = Row+WEST;															//first spot to start checking the row
 			for(j=Column + SOUTH;j <= 7;j += SOUTH){ 	//Column
 				if(i<0){																	// if the row is outside the array: stop
 					break southwest;
 				}
 				if(board[i][j] == PlayerTurn){ 		// looks for a piece to flip between
-					if(makeMove ==true){
-						for(j=j; j >Column; j -= SOUTH){	//flip pieces between player color and placed piece
-							board[i][j] = PlayerTurn;
-							i-=WEST;
-						}
-					}
-					validMove = true;
-					break southwest;
+					inArow++;
 				}
 				else if(board[i][j] == EMPTY){		//if an empty space is found none are flipped
 					break southwest;
 				}
 				i += WEST;																//Increments the row
+				if(inArow == 4){
+					winMove = true;
+				}
 			}
 		}
 			
+		inArow =0;
+		
 		southeast:
-		if((Column + SOUTH <= 7) && (Row + EAST <= 7) && (board[Row+EAST][Column + SOUTH] == -1*PlayerTurn)){
+		if((Column + SOUTH <= 7) && (Row + EAST <= 7) && (board[Row+EAST][Column + SOUTH] == PlayerTurn)){
+			inArow++;
 			i = Row+EAST;															//first spot to start checking the row
 			for(j=Column + SOUTH;j <= 7;j += SOUTH){ 	//Column
 				if(i>7){																	// if the row is outside the array: stop
 					break southeast;
 				}
 				if(board[i][j] == PlayerTurn){ 		// looks for a piece to flip between
-					if(makeMove ==true){
-						for(j=j; j >Column; j -= SOUTH){	//flip pieces between player color and placed piece
-							board[i][j] = PlayerTurn;
-							i-=EAST;
-						}
-					}
-					validMove = true;
-					break southeast;
+					inArow++;
 				}
 				else if(board[i][j] == EMPTY){		//if an empty space is found none are flipped
 					break southeast;
 				}
 				i += EAST;																//Increments the row
+				if(inArow == 4){
+					winMove = true;
+				}
 			}
 		}
+		
+		inArow =0;
 		
 		west:
-		if((Row + WEST >= 0) && (board[Row + WEST][Column] == -1*PlayerTurn)){
+		if((Row + WEST >= 0) && (board[Row + WEST][Column] ==PlayerTurn)){
+			inArow++;
 			for(i=Row + WEST;i >= 0;i += WEST){ 	//Row
 				if(board[i][Column] == PlayerTurn){ 		// looks for a piece to flip between
-					if(makeMove ==true){
-						for(i=i; i <Row; i -= WEST){	//flip pieces between player color and placed piece
-							board[i][Column] = PlayerTurn;
-						}
-					}
-					validMove = true;
-					break west;
+					inArow++;
 				}
 				else if(board[i][Column] == EMPTY){		//if an empty space is found none are flipped
 					break west;
 				}
+				if(inArow == 4){
+					winMove = true;
+				}
 			}
 		}
+		
+		inArow =0;
 		
 		east:
-		if((Row + EAST <= 7) && (board[Row + EAST][Column] == -1*PlayerTurn)){
+		if((Row + EAST <= 7) && (board[Row + EAST][Column] == PlayerTurn)){
+			inArow++;
 			for(i=Row + EAST;i <= 7;i += EAST){ 	//Row
 				if(board[i][Column] == PlayerTurn){ 		// looks for a piece to flip between
-					if(makeMove ==true){
-						for(i=i; i >Row; i -= EAST){	//flip pieces between player color and placed piece
-							board[i][Column] = PlayerTurn;
-						}
-					}
-					validMove = true;
-					break east;
+					inArow++;
 				}
 				else if(board[i][Column] == EMPTY){		//if an empty space is found none are flipped
 					break east;
 				}
+				if(inArow == 4){
+					winMove = true;
+				}
 			}
 		}
 		
-		return validMove;
+		return winMove;
 	}
 	
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
 		g.setColor(Color.black);
 		int blackPieces = 0;
-		int whitePieces = 0;
+		int REDPieces = 0;
 		int i,j;
 		for(i=0;i<8;i++){ //Row
 			for(j=0;j<8;j++){ //Column	
@@ -313,9 +310,9 @@ public class Othello extends JComponent {
 					blackPieces++;
 				}
 				else if(board[i][j] == -1){
-					g.setColor(Color.WHITE);;
+					g.setColor(Color.RED);;
 					g.fillOval((getWidth()-tileWidth*8)/2+tileDiff+  i*tileWidth,tileWidth*2+  tileDiff + j*tileWidth, tileWidth-2*tileDiff, tileWidth-2*tileDiff);
-					whitePieces++;
+					REDPieces++;
 				}
 					//needs to draw each piece as a filled color circle   board[i][j] 
 			} 
@@ -323,28 +320,24 @@ public class Othello extends JComponent {
 		g.setColor(Color.black);
 		g.setFont(new Font("Arial", Font.BOLD,tileWidth/2));
 		
-		if(blackPieces ==0){
-			g.drawString("White Wins!",(getWidth()-tileWidth*8)/2+tileDiff,tileWidth);
-		}
-		else if(whitePieces == 0){
-			g.drawString("Black Wins!",(getWidth()-tileWidth*8)/2+tileDiff,tileWidth);
-		}
-		else if(blackPieces + whitePieces == 64 || GameState == 0){
-			if(blackPieces > whitePieces){
+			if(GameState == RED ){
+				g.drawString("Red Wins!",(getWidth()-tileWidth*8)/2+tileDiff,tileWidth);
+			}
+			else if(GameState == BLACK ){
 				g.drawString("Black Wins!",(getWidth()-tileWidth*8)/2+tileDiff,tileWidth);
 			}
-			else{
-				g.drawString("White Wins!",(getWidth()-tileWidth*8)/2+tileDiff,tileWidth);
+			else if(GameState == 2 ){
+				g.drawString("Tie!",(getWidth()-tileWidth*8)/2+tileDiff,tileWidth);
 			}
-		}
-		else{
-			if(PlayerTurn == -1){
-				g.drawString("White Turn",(getWidth()-tileWidth*8)/2+tileDiff,tileWidth);
+			else if(PlayerTurn == -1){
+				g.drawString("RED Turn",(getWidth()-tileWidth*8)/2+tileDiff,tileWidth);
 			}
 			else if(PlayerTurn == 1){
 				g.drawString("Black Turn",(getWidth()-tileWidth*8)/2+tileDiff,tileWidth);
 			}
-		}
+			if(GameState != 0){
+				PlayerTurn =0;
+			}
 
 	}
 }
--- src/GameLauncher.java
+++ src/GameLauncher.java
@@ -84,7 +84,38 @@ private String GameType = "";
 			GameType = "Othello";
 		}
 		else if(X.getActionCommand()=="Connect Four"){
-			
+			 setTitle("Connect Four");
+			    getContentPane().removeAll();
+			    setResizable(false);
+			    setVisible(true);	
+				repaint();
+			    int tileWidth = getContentPane().getHeight()/12;
+				ConnectFour game = new ConnectFour();
+				ConnectFour.setup(tileWidth);
+				getContentPane().removeAll();
+				getContentPane().setBackground(Color.blue);
+				
+				//sets up the button configuration for Othello
+				System.out.println(tileWidth);
+				for(int i=0;i<8;i++){ //Row
+					for(int j=0;j<8;j++){ //Column	
+						tiles[i][j] = new JButton(Integer.toString(i)+Integer.toString(j));
+						tiles[i][j].setLocation((getContentPane().getWidth()-tileWidth*8)/2+ i*tileWidth,tileWidth*2+ j*tileWidth);
+						tiles[i][j].setSize(tileWidth,tileWidth);
+						tiles[i][j].setBorder(BorderFactory.createLineBorder(Color.yellow,2));
+						tiles[i][j].setActionCommand(Integer.toString(i)+Integer.toString(j));
+						tiles[i][j].addActionListener(this);
+						tiles[i][j].setText("");
+						tiles[i][j].setOpaque(false);
+						tiles[i][j].setContentAreaFilled(false);
+						add(tiles[i][j]);
+					}
+				}
+				
+				add(game);
+				setVisible(true);	
+				repaint();
+				GameType = "Connect Four";
 		}
 		
 		
@@ -96,7 +127,9 @@ private String GameType = "";
 			repaint();
 		}
 		else if(GameType == "Connect Four"){
-			
+			ConnectFour.attemptMove(X.getActionCommand());
+			setVisible(true);
+			repaint();
 		}
 	}
 }
